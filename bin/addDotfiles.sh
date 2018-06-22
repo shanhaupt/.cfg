ee6e18605571bdffdff3c447ea35d332a6abb41c
Shan Haupt <shaupt@usc.edu>
2018-06-21 18:36:42 -0700

    Pushed .bashrc .bash_aliases .vimrc .gitconfig addDotfiles.sh getDotfiles.sh

--- bin/addDotfiles.sh
+++ bin/addDotfiles.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-sudo rm -f .bashrc
-sudo cp ../.bashrc .bashrc
 
-sudo rm -f .bash_aliases
-sudo cp ../.bash_aliases .bash_aliases
+CFGPATH=~/.cfg/
+HOME=~/
+sudo rm -f $CFGPATH/.bashrc
+sudo cp ~/.bashrc $CFGPATH/.bashrc
+
+sudo rm -f $CFGPATH/.bash_aliases
+sudo cp ~/.bash_aliases $CFGPATH/.bash_aliases
 
 
-sudo rm -f .vimrc
-sudo cp ../.vimrc .vimrc
+sudo rm -f $CFGPATH/.vimrc
+sudo cp ~/.vimrc $CFGPATH/.vimrc
 
-sudo rm -f .gitconfig
-sudo cp ../.gitconfig .gitconfig
+sudo rm -f $CFGPATH/.gitconfig
+sudo cp ~/.gitconfig $CFGPATH/.gitconfig
 
 git add .bashrc
 git add .bash_aliases
