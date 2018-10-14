061a9aec38a4ee64b797fb2993dc6dbd9cdfa2bc
Shan Haupt <shaupt@usc.edu>
2018-10-13 23:44:30 -0700

    Added powerline install scripts

---
+++ bin/installPowerline.sh
@@ -0,0 +1,10 @@
+sudo apt-get install python-pip git
+pip install --user git+git://github.com/Lokaltog/powerline
+
+#Note if this does not work then do the following:
+#append:
+#   if [ -d "$HOME/.local/bin" ]; then
+#        PATH="$HOME/.local/bin:$PATH"
+#   fi
+# to ~/.profile
+
